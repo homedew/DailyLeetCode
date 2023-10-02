c061a04035f8056f03b0d40c691d9b523f41100b
xhomedew <homedew.tc@gmail.com>
2023-10-02 21:24:18 +0700

    Using queue

---
+++ LeetCodeDaily/933. Number of Recent Calls.cs
@@ -0,0 +1,23 @@
+public class RecentCounter {
+
+    private Queue<int> queue;
+    public RecentCounter() {
+        queue = new Queue<int>();
+    }
+    
+    public int Ping(int t) {
+        queue.Enqueue(t);
+        while(queue.Peek() < t - 3000)
+        {
+            queue.Dequeue();
+        }
+
+        return queue.Count();
+    }
+}
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter obj = new RecentCounter();
+ * int param_1 = obj.Ping(t);
+ */
