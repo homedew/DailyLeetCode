d34ae63c5dc6ab8991a50d7a6a0847642d2bec9b
xhomedew <homedew.tc@gmail.com>
2023-10-03 07:56:46 +0700

    slow fast technique

--- LeetCodeDaily/2095. Delete the Middle Node of a Linked List.cs
+++ LeetCodeDaily/2095. Delete the Middle Node of a Linked List.cs
@@ -10,31 +10,33 @@
  *     }
  * }
  */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
 public class Solution {
     public ListNode DeleteMiddle(ListNode head) {
+        if(head.next ==null) return null;
 
-        if(head.next == null) return null;
-        int count = 0;
-        ListNode p1 = head; // 1 pass
-        ListNode p2 = head; // 2 pass
+        ListNode slow = head;
+        ListNode fast = head.next.next;
 
-        while(p1 != null)
+        while(fast != null && fast.next != null)
         {
-            count++;
-            p1 = p1.next;
+            slow = slow.next;
+            fast = fast.next.next;
         }
 
-        int middle = count / 2;
-
-        Console.WriteLine(middle);
-
-        for(int i = 0 ; i < middle - 1; i++)
-        {
-            p2 = p2.next;
-        }
-
-        p2.next = p2.next.next;
+        slow.next = slow.next.next;
 
         return head;
+
     }
 }
