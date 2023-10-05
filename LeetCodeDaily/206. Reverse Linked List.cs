4688b629cc2aac37bd9a10f886a895d2fc2a2d84
xhomedew <homedew.tc@gmail.com>
2023-10-05 22:04:19 +0700

    Interative linkedlist problem

---
+++ LeetCodeDaily/206. Reverse Linked List.cs
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/reverse-linked-list
+
+// Using iterative 
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode ReverseList(ListNode head) {
+        ListNode prevNode = null;
+        ListNode curr = head;
+
+        while(curr != null)
+        {
+            ListNode nextTemp = curr.next;
+            curr.next = prevNode;
+            prevNode = curr;
+            curr = nextTemp;
+        }
+
+        return prevNode;
+    }
+}
+
+// Another solution
