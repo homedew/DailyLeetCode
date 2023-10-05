f8121f7fc692d1fcf7e30d082379bf79b6cd391c
xhomedew <homedew.tc@gmail.com>
2023-10-06 00:00:50 +0700

    Magic in code

--- LeetCodeDaily/206. Reverse Linked List.cs
+++ LeetCodeDaily/206. Reverse Linked List.cs
@@ -29,4 +29,26 @@ public class Solution {
     }
 }
 
-// Another solution
+// Another solution using recursive
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode ReverseList(ListNode head) {
+        if(head == null || head.next == null) return head;
+
+        ListNode p = ReverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        
+        return p;
+    }
+}
