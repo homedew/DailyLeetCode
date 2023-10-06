0c79a57512ca1233fc07dfc3336475227214ae39
xhomedew <homedew.tc@gmail.com>
2023-10-06 23:02:45 +0700

    DFS for Binary tree

---
+++ LeetCodeDaily/104. Maximum Depth of Binary Tree.cs
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/maximum-depth-of-binary-tree/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public int MaxDepth(TreeNode root) {
+         if(root == null) return 0;
+
+         int leftHeight = MaxDepth(root.left);
+         int rightHeight = MaxDepth(root.right);
+
+         return Math.Max(leftHeight, rightHeight) + 1;
+    }
+}
