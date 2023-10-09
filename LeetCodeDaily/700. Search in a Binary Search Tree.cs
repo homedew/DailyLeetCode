826903d1975e67fa70dbf4619d6790e22583171f
xhomedew <homedew.tc@gmail.com>
2023-10-09 23:21:18 +0700

    Update solution O 1 space

--- LeetCodeDaily/700. Search in a Binary Search Tree.cs
+++ LeetCodeDaily/700. Search in a Binary Search Tree.cs
@@ -24,3 +24,27 @@ public class Solution {
         }
     }
 }
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public TreeNode SearchBST(TreeNode root, int val) {
+      while(root !=null && root.val != val)
+      {
+          root = val < root.val ? root.left ? root.right;
+      }
+
+      return root;
+    }
+}
