039a8940a91ced09dc786ff7d62e9a10e1a497b9
xhomedew <homedew.tc@gmail.com>
2023-10-09 22:36:15 +0700

    Find root in BST

---
+++ LeetCodeDaily/700. Search in a Binary Search Tree.cs
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/search-in-a-binary-search-tree/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public TreeNode SearchBST(TreeNode root, int val) {
+        if(root == null || root.val == val) return root;
+
+        if(val <root.val) {
+            return SearchBST(root.left, val);
+        } else {
+            return SearchBST(root.right, val);
+        }
+    }
+}
