ecc68ee2ace090cedd89bc77ceeb6dfc15b2704a
xhomedew <homedew.tc@gmail.com>
2023-10-12 00:35:38 +0700

    Update using stack

--- LeetCodeDaily/841. Keys and Rooms.cs
+++ LeetCodeDaily/841. Keys and Rooms.cs
@@ -25,3 +25,29 @@ public class Solution {
 
 
 // [[1,3],[3,0,1],[2],[0]]
+
+public class Solution {
+    public bool CanVisitAllRooms(IList<IList<int>> rooms) {
+        bool[] visisted = new bool[rooms.Count];   
+        visisted[0] = true;
+
+        Stack<int> room = new Stack<int>();
+        room.Push(0);
+
+        while(room.Any())
+        {
+            int key = room.Pop();
+           
+            foreach(int k in rooms[key])
+            {
+                if(!visisted[k])
+                {
+                    visisted[k] = true;
+                    room.Push(k);
+                }
+            }
+        }
+        return visisted.All(v=> v);
+        
+    }
+}
